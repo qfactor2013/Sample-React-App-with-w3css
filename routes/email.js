ba3b98b91ab94ac92e6664e1e09c70887106fefd
qfactor2013 <qfactor2013@gmail.com>
2021-03-30 02:19:15 +0900

    modified email.js  change SMTP Server Sendgrid -> Google

--- routes/email.js
+++ routes/email.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
-// const sgTransport  = require('nodemailer-sendgrid-transport');
 
 /* post email listing. */
 router.post('/', function (req, res, next) {
-    
     // console.log(req);
     // console.log(req.body);
     // console.log(req.body.name)
@@ -14,12 +12,13 @@ router.post('/', function (req, res, next) {
     // console.log(req.body.subject)
     // console.log(req.body.message)
     // console.log(req.data);
-    // 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
+
     let transporter = nodemailer.createTransport(smtpTransport({
-        service: 'SendGrid',
+        service: 'google',
+        host: 'smtp.gmail.com',
         auth: {
-            user: process.env.SEND_GRID_ID,
-            pass: process.env.SEND_GRID_PW,
+            user: process.env.GOOGLE_ID,
+            pass: process.env.GOOGLE_PW,
         }       
     }))
     
@@ -36,6 +35,7 @@ router.post('/', function (req, res, next) {
         }
         console.log('Message %s sent: %s', info.messageId, info.response)
     })
+
     res.sendStatus(200);
 });
 
